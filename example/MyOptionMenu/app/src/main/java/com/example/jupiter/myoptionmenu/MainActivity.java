9f8eb37ed6389cbd9ce772019234f5899df78176
Lee DongUk <>
2018-06-18 17:49:45 +0900

    no message

---
+++ example/MyOptionMenu/app/src/main/java/com/example/jupiter/myoptionmenu/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.jupiter.myoptionmenu;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar abar = getSupportActionBar();
+        //abar.hide();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int curId = item.getItemId();
+        switch (curId) {
+            case R.id.menu_refresh:
+                Toast.makeText(this, "새로고침", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this, "검색", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(this, "설정", Toast.LENGTH_LONG).show();
+                break;
+
+                default:
+                    break;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
